2e32cc59c3cfe51cbf4a9125a63967bfb3a02898
ufoo68 <mitarou.yuutarou@gmail.com>
2019-08-09 11:58:12 +0900

    Updates

--- precache-manifest.a9128dce93de475f0b8653aa9d382550.js
+++ precache-manifest.a9128dce93de475f0b8653aa9d382550.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4416a7014c36bb2badaa329fda9ccb5b",
+    "revision": "51efd514b6460d3e23810ebc7d80456c",
     "url": "/compass-panda-compass/index.html"
   },
   {
-    "revision": "621a04944eadf4f92912",
+    "revision": "752617a43c9abcfb2335",
     "url": "/compass-panda-compass/static/css/main.09da2d74.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/compass-panda-compass/static/js/2.0f2ef80c.chunk.js"
   },
   {
-    "revision": "621a04944eadf4f92912",
-    "url": "/compass-panda-compass/static/js/main.f0f88de3.chunk.js"
+    "revision": "752617a43c9abcfb2335",
+    "url": "/compass-panda-compass/static/js/main.1311e8df.chunk.js"
   },
   {
     "revision": "7fd1211fd545e9240595",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/compass-panda-compass/precache-manifest.37ce5bb6f3ee931d9b7f2cc1c9acbecf.js"
+  "/compass-panda-compass/precache-manifest.a9128dce93de475f0b8653aa9d382550.js"
 );
 
 self.addEventListener('message', (event) => {
