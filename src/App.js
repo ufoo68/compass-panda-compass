b9137811db8703303605bdd34541f59d567bb7b8
ufoo68 <mitarou.yuutarou@gmail.com>
2019-08-09 12:07:24 +0900

    [add]compass with image of panda

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import DeviceOrientation from 'react-device-orientation';
+import "./App.css"
+import panda from './assets/panda.png';
 
 function App() {
   return (
-<DeviceOrientation>
-    {({ absolute, alpha, beta, gamma }) => (
-      <div>
-        {`Absolute: ${absolute}`}
-        {`Alpha: ${alpha}`}
-        {`Beta: ${beta}`}
-        {`Gamma: ${gamma}`}
+  <DeviceOrientation>
+    {({ alpha }) => (
+      <div class="Wrap">
+        <img src={panda} alt="panda" style={{transform: `rotate(${alpha.toFixed(0)}deg)`}} width="300"/>
       </div>
     )}
   </DeviceOrientation>
